2f4714b7109d03378db38740ef8c766d5c142af7
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-04-15 21:25:36 -0400

    Remove comment from download_and_extract

--- download_and_extract.py
+++ download_and_extract.py
@@ -7,5 +7,3 @@ import lobby.generator as generator
 print("Starting Lobbyist Registry Downloader")
 Downloader().extract_files()
 print("Finished Lobbyist Registry Downloader")
-
-#log = {"Version": "1.0.0", "Datetime":  "Author": "Lobbyist Registry Downloader"}
