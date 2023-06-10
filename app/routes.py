88b589a336a09fa543493a096f5089e51ce3f0fe
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-06-10 06:47:34 +0000

    Get Flask Runing

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,13 @@ from app.models import LobbyingReport
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/lobbying_reports')
+def lobbying_reports():
+    reports = LobbyingReport.query.all()
+    return render_template('lobbying_reports.html', title='Lobbying Reports', reports=reports)
+
+@app.route('/lobbying_report/<int:id>')
+def lobbying_report(id):
+    report = LobbyingReport.query.get(id)
+    return render_template('lobbying_report.html', title='Lobbying Report', report=report)
