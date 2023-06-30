dcee9c950b4e4d8c4001d235a3c0e9ab2ae671ea
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-06-30 23:26:43 +0000

    Dedup address

--- build/beneficiaries.py
+++ build/beneficiaries.py
@@ -1,23 +1,25 @@
 from datetime import datetime
 from typing import List
 from sqlalchemy.orm import Session
-from app.models.models import Beneficiary, LobbyingReport
+from app.models.models import Beneficiary, raw_address_address
 from app.models.processor_models import RawBeneficiary
 from app.models.enums import BeneficiaryType
 from sqlalchemy import insert
 
-def get_beneficiary_data_row(raw_beneficiary: RawBeneficiary) -> dict:
+def get_beneficiary_data_row(raw_beneficiary: RawBeneficiary, address_lookup: dict[int,int]) -> dict:
     return {
         "type": BeneficiaryType(raw_beneficiary.Type),
         "name": raw_beneficiary.Name,
         "trade_name": raw_beneficiary.TradeName,
-        "report_id":raw_beneficiary.report_id
+        "address_id": address_lookup.get(raw_beneficiary.address_id), 
+        "report_id": raw_beneficiary.report_id
     }
 
-
 def create_beneficiaries_table(session: Session, raw_beneficiaries: List[RawBeneficiary]) -> List[Beneficiary]:
-    data = [get_beneficiary_data_row(raw_beneficiary) for raw_beneficiary in raw_beneficiaries]
+    address_lookup = {mapping.raw_address_id: mapping.address_id for mapping in session.query(raw_address_address).all()}
 
-    session.execute(insert(Beneficiary), data)
+    data = [get_beneficiary_data_row(raw_beneficiary, address_lookup) for raw_beneficiary in raw_beneficiaries]
+
+    session.execute(insert(Beneficiary).values(data))
     session.commit()
     return session.query(Beneficiary).all()
