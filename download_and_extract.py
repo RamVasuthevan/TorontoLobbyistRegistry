c40aacac14b65501d6a05bf8fc19fd8c2518e6b9
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-07-01 01:35:00 +0000

    Revert "Add Firms"
    This reverts commit c087c4e620ad6187e2745405a37a359bda3963ab.

--- download_and_extract.py
+++
@@ -1,9 +0,0 @@
-from lobby import *
-import pprint as pp
-import json
-from lobby.downloader import Downloader
-
-
-print("Starting Lobbyist Registry Downloader")
-Downloader().extract_files()
-print("Finished Lobbyist Registry Downloader")
