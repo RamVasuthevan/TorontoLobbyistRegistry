7be1576ef69d4fa62005a94c47584ce07a4513d0
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-07-21 05:28:25 +0000

    Blacked

--- lobby/downloader.py
+++ lobby/downloader.py
@@ -11,13 +11,15 @@ API_BASE_URL = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
 API_PACKAGE_SHOW_URL = API_BASE_URL + "/api/3/action/package_show"
 API_PACKAGE_ID = "lobbyist-registry"
 
-def get_package()-> Dict:
-        """Returns response from Toronto Open Data CKAN API"""
 
-        params = {"id": API_PACKAGE_ID}
-        package = requests.get(API_PACKAGE_SHOW_URL, params=params).json()
+def get_package() -> Dict:
+    """Returns response from Toronto Open Data CKAN API"""
 
-        return package
+    params = {"id": API_PACKAGE_ID}
+    package = requests.get(API_PACKAGE_SHOW_URL, params=params).json()
+
+    return package
+
 
 if __name__ == "__main__":
     package = get_package()
@@ -25,14 +27,18 @@ if __name__ == "__main__":
 
     with open(OPEN_DATA_RESPONSE_FILE_NAME, "w") as json_file:
         json_file.write(json.dumps(package, indent=4))
-    
+
     resource_response = {}
     for resource in package["result"]["resources"]:
-        resource_response[f"{resource['name']}.{resource['format'].lower()}"] =  requests.get(resource["url"])
-    
-    for resource,response in resource_response.items():
+        resource_response[
+            f"{resource['name']}.{resource['format'].lower()}"
+        ] = requests.get(resource["url"])
+
+    for resource, response in resource_response.items():
         with open(resource, "wb") as f:
             f.write(response.content)
-    
-    lobbyactivity_zip = zipfile.ZipFile(BytesIO(resource_response[LOBBY_ACTIVITY_FILE_NAME].content))
+
+    lobbyactivity_zip = zipfile.ZipFile(
+        BytesIO(resource_response[LOBBY_ACTIVITY_FILE_NAME].content)
+    )
     lobbyactivity_zip.extractall()
