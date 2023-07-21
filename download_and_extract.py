bacc8cc31a59fa236f7bf8582814536191bba867
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-07-21 06:07:01 +0000

    Delete download_and_extract.py

--- download_and_extract.py
+++
@@ -1,7 +0,0 @@
-from lobby import *
-from lobby.downloader import Downloader
-
-
-print("Starting Lobbyist Registry Downloader")
-Downloader().extract_files()
-print("Finished Lobbyist Registry Downloader")
