12b6d60c578b33f9f223aa78ae1aaf9d0b9292a4
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-08-26 16:59:57 -0400

    Refactor Downloader (#86)
    * Remove cache
    
    * Removed load_from_url
    
    * Remove _called
    
    * Remove magic variables from class
    
    * Delete metadata_dates
    
    * Remove last_modified
    
    * Remove lobbyactivity_xml
    
    * Convert to procedural
    
    * Delete downloader class
    
    * Get working
    
    * Extract hardcoded values
    
    * Update to use downloader.py
    
    * Blacked
    
    * Rename folder to scripts
    
    * Rename to download_data
    
    * Update to run from scripts/download_data.py
    
    * Remove file names
    
    * Extract into function downdload_data()
    
    * Fix typo and Blacked
    
    * Add github-actions
    
    * Delete download_and_extract.py
    
    * Create upload_to_cloudflare
    
    * Blacked

--- config.py
+++ config.py
@@ -9,9 +9,15 @@ class Config(object):
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = ["your-email@example.com"]
+
+    CLOUDFLARE_ACCOUNT_ID = os.environ.get("CLOUDFLARE_ACCOUNT_ID")
+    CLOUDFLARE_ACCESS_KEY_ID = os.environ.get("CLOUDFLARE_ACCESS_KEY_ID")
+    CLOUDFLARE_SECRET_ACCESS_KEY = os.environ.get("CLOUDFLARE_SECRET_ACCESS_KEY")
+    CLOUDFLARE_R2_BUCKET_NAME = os.environ.get("BUCKET_NAME")
