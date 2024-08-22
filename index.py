2c2b0280ad17ecd384f067f559bc7e2df9037a5c
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2024-08-22 19:30:25 -0400

    Copy over index.py

---
+++ index.py
@@ -0,0 +1,32 @@
+import asyncio
+from datasette.app import Datasette
+import json
+import pathlib
+import os
+
+static_mounts = [
+    (static, str((pathlib.Path(".") / static).resolve()))
+    for static in []
+]
+
+metadata = dict()
+try:
+    metadata = json.load(open("metadata.json"))
+except Exception:
+    pass
+
+secret = os.environ.get("DATASETTE_SECRET")
+
+true, false = True, False
+
+ds = Datasette(
+    [],
+    ["lobbyist_registry.db"],
+    static_mounts=static_mounts,
+    metadata=metadata, template_dir="templates",
+    secret=secret,
+    cors=True,
+    settings={}
+)
+asyncio.run(ds.invoke_startup())
+app = ds.app()
