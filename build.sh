49a1f0b90ceb8da12ec7ea5f44e3e3ab3a169b7d
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2024-08-22 21:17:37 -0400

    export bin again

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-export PATH=/python312/lib/python3.12/bin
+export PATH=/python312/lib/python3.12/bin:$PATH
 python3 --version
 pip3 install pipenv
 pip3 show pipenv
 which pipenv
+pipenv install
+pipenv run python main.py
