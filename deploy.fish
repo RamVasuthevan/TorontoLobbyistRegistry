54965a6ee4b224c013f8b21078ce425ffc3753d3
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2024-10-11 23:37:25 -0400

    It works

--- deploy.fish
+++ deploy.fish
@@ -2,6 +2,6 @@
 
 set -x SERVER_IP 192.34.62.11
 pipenv install
-pipenv run main.py
+pipenv run python main.py
 rsync -avz --delete ./ root@$SERVER_IP:/root/TorontoLobbyistRegistry
 ssh root@$SERVER_IP "systemctl restart TorontoLobbyistRegistry"
